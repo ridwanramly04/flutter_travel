92f7bd913777d3760d4787f0ae4c19f68c211a03
ridwanramly04 <pedaslabs@gmail.com>
2019-12-01 23:45:19 +0800

    minute 37:21

---
+++ lib/screens/destination_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel/models/destination_model.dart';
+
+class DestinationScreen extends StatefulWidget {
+
+  final Destination destination;
+
+  @override
+  _DestinationScreenState createState() => _DestinationScreenState();
+}
+
+class _DestinationScreenState extends State<DestinationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
